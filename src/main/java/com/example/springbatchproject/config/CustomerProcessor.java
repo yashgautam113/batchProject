dba1b458ccec93e8c79c2519a157624bd687e93b
yashgautam113 <yash.gautam113@gmail.com>
2024-03-04 11:58:02 +0530

    Initial commit

---
+++ src/main/java/com/example/springbatchproject/config/CustomerProcessor.java
@@ -0,0 +1,11 @@
+package com.example.springbatchproject.config;
+
+import com.example.springbatchproject.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        return customer;
+    }
+}
---
+++ src/main/java/com/example/springbatchproject/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.example.springbatchproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CUSTOMER_INFO")
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "GENDER")
+    private String gender;
+    @Column(name = "CONTACT")
+    private String contactNo;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "DOB")
+    private String dob;
+}
